628ebbdae47ccee8ee38c4e02589f5b6780c1136
UrosPurtic <uros.purtic@gmail.com>
2018-04-27 21:50:42 +0200

    zadatak 7

--- src/pages/Products.vue
+++ src/pages/Products.vue
@@ -1,12 +1,35 @@
 <template>
-    <div>
-            <ul v-for="(product,key) in products" :key="product.id">
-                <li>{{ product.title }}</li>
-                <li>{{ product.company }}</li>
+    <div style="padding-top: 15px;">
+
+        <label>Search products by name</label>
+        <input v-model="searchTerm" type="text" />
+
+        <table class="table">
+            <thead>
+            <th scope="col">Product name</th>
+            <th scope="col">Product company</th>
+            <th scope="col">Current amount available</th>
+            <th scope="col">Add</th>
+            <th scope="col">Remove</th>
 
 
+            </thead>
 
-            </ul>
+            <tbody>
+            <tr v-for="(product,key) in filteredProducts" :key="product.id">
+                <td>{{ product.title }}</td>
+                <td>{{ product.company}}</td>
+                <td style="text-align: center">{{ product.available}}</td>
+                <td><button @click="addAmount(product)" class="btn btn-secundary">+ </button></td>
+
+                <td><button v-if="product.available" @click="removeAmount(product)"  class="btn btn-secundary">-</button></td>
+
+            </tr>
+            </tbody>
+
+
+        </table>
+
 
 
     </div>
@@ -24,9 +47,36 @@
 
             return {
 
-                products: productService.list()
+                products: productService.list(),
+                searchTerm : ''
 
             }
+
+        },
+
+        computed: {
+
+            filteredProducts() {
+                return this.products.filter(product => {
+                    return product.title.toLowerCase().startsWith(this.searchTerm.toLowerCase())
+                })
+            }
+        },
+
+        methods: {
+
+            addAmount(product){
+
+             productService.addAmount(product)
+            },
+
+            removeAmount(product){
+
+             productService.removeAmount(product)
+
+            }
+
+
 
         }
     }
--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -3,24 +3,34 @@ const products = [
     {   id: 1 ,
         title: 'Plazma',
         company : 'Bamby',
+        available : 10
     },
 
 
     {   id: 2 ,
         title: 'Oreo',
         company : 'GreatCompany',
+        available : 8
     },
 
 
     {   id: 3 ,
         title: 'Nutella',
         company : 'BestCompanyEver',
+        available : 35
     },
 
 
     {   id: 4 ,
         title: 'Noblice',
         company : 'Stark',
+        available : 5
+    },
+
+    {   id: 5 ,
+        title: 'Plazma cokolada',
+        company : 'Bamby',
+        available : 45
     },
 
 
@@ -37,6 +47,18 @@ export default class ProductService {
     list(){
 
         return products
+    }
+
+    addAmount(product){
+
+        product.available++
+
+    }
+
+    removeAmount(product){
+
+        product.available--
+
     }
 
 
